a50844120dd4220df408a51638bd821a49d22102
Rafal Klodzinski <rafalklo@gmail.com>
2011-03-23 17:56:32 -0400

    Modify the routes to accept the permalink and show action link to permalink

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.pages 'pages/:permalink', :controller => 'pages', :action => 'show'
   map.resources :pages
   map.namespace :admin do |admin|
       admin.resources :pages
